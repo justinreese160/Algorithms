ff2ebae55dead63623ec7df938aa26c8b3f45a9a
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-09-29 13:17:52 -0500

    added new algos

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+console.log('CODE EVERYDAY!🦄 👩🏼‍💻 🌈');
+
+// sandbox to do new challenges in and and use with sandbox.html
+// to test in the browser console.log
+// when you get it right, create a new js file and paste in there!
